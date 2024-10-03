64a924bc9a0d58079c90eec6685487d9c6d0bf3a
Elliott-Roach <elliott.roach@mths.ca>
2024-10-03 10:51:30 -0400

    add colure coment

--- main.ts
+++ main.ts
@@ -12,36 +12,43 @@ pins.digitalWritePin(DigitalPin.P15, 0)
 pause(1000)
 
 input.onButtonPressed(Button.A, function () {
+    /*blue*/
     pins.digitalWritePin(DigitalPin.P13, 1)
     pause(1000)
     pins.digitalWritePin(DigitalPin.P13, 0)
     pause(1000)
+    /*grean*/
     pins.digitalWritePin(DigitalPin.P14, 1)
     pause(1000)
     pins.digitalWritePin(DigitalPin.P14, 0)
     pause(1000)
+    /*red*/
     pins.digitalWritePin(DigitalPin.P15, 1)
     pause(1000)
     pins.digitalWritePin(DigitalPin.P15, 0)
     pause(1000)
+    /*light blue*/
     pins.digitalWritePin(DigitalPin.P13, 1)
     pins.digitalWritePin(DigitalPin.P14, 1)
     pause(1000)
     pins.digitalWritePin(DigitalPin.P13, 0)
     pins.digitalWritePin(DigitalPin.P14, 0)
     pause(1000)
+    /*perpol*/
     pins.digitalWritePin(DigitalPin.P13, 1)
     pins.digitalWritePin(DigitalPin.P15, 1)
     pause(1000)
     pins.digitalWritePin(DigitalPin.P13, 0)
     pins.digitalWritePin(DigitalPin.P15, 0)
     pause(1000)
+    /*dark grean*/
     pins.digitalWritePin(DigitalPin.P15, 1)
     pins.digitalWritePin(DigitalPin.P14, 1)
     pause(1000)
     pins.digitalWritePin(DigitalPin.P15, 0)
     pins.digitalWritePin(DigitalPin.P14, 0)
     pause(1000)
+    /*wight*/
     pins.digitalWritePin(DigitalPin.P15, 1)
     pins.digitalWritePin(DigitalPin.P14, 1)
     pins.digitalWritePin(DigitalPin.P13, 1)
@@ -49,4 +56,5 @@ input.onButtonPressed(Button.A, function () {
     pins.digitalWritePin(DigitalPin.P15, 0)
     pins.digitalWritePin(DigitalPin.P14, 0)
     pins.digitalWritePin(DigitalPin.P13, 0)
+    pause(1000)
 })
